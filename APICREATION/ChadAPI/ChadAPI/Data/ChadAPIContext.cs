2003023a3dc491f06c67b7608f360ec141e68131
ZianYangAng <bryanang1998@gmail.com>
2018-11-18 11:11:56 +1300

    copied code

---
+++ APICREATION/ChadAPI/ChadAPI/Data/ChadAPIContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChadAPI.Models
+{
+    public class ChadAPIContext : DbContext
+    {
+        public ChadAPIContext (DbContextOptions<ChadAPIContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ChadAPI.Models.MemeItem> MemeItem { get; set; }
+    }
+}
